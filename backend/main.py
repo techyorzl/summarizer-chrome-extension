e407b0e85046fcd09d30fab0b573ace1e0ea019e
techyorzl <baryush.w@gmail.com>
2025-02-17 20:38:53 +0530

    Updated main.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
+import requests
+
 from logggerrr import setup_logger
-from model.model_api import generate_summary
 
 app = FastAPI()
 
 logger = setup_logger('backend', 'backend.log')
+
+MODEL_API_URL = "http://model:5000"
 
 class TextRequest(BaseModel):
     text: str
@@ -16,9 +19,12 @@ async def summarize_text(request: TextRequest):
         raise HTTPException(status_code=400, detail="Text cannot be empty.")
 
     try:
-        summary = await generate_summary(request.text)
-        return {"summary": summary}
+        response = requests.post(f"{MODEL_API_URL}/generate_summary", json={"text": request.text})
+        if response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Model server error")
+
+        return {"summary": response.json()}
+    
     except Exception as e:
         logger.error(f"Error processing request: {e}")
         raise HTTPException(status_code=500, detail="Internal server error")
-
